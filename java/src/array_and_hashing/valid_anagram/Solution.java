3bcb672d0a7c0840ce1bde71096c217d9858d3f0
etornamklu <03etornam@gmail.com>
2026-01-17 13:08:31 +0000

    valid anagram

---
+++ java/src/array_and_hashing/valid_anagram/Solution.java
@@ -0,0 +1,21 @@
+package array_and_hashing.valid_anagram;
+
+
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int val : count){
+            if(val != 0) return false;
+        }
+        return true;
+    }
+}
