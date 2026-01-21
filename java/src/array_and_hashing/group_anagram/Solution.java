ae1792b8a69e3c87b9af4a1ab0f2edaf0d3611e5
etornamklu <03etornam@gmail.com>
2026-01-21 07:52:46 +0000

    group anagram

---
+++ java/src/array_and_hashing/group_anagram/Solution.java
@@ -0,0 +1,22 @@
+package array_and_hashing.group_anagram;
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            int[] distribution = new int[26];
+            for(char c : s.toCharArray()){
+                distribution[c - 'a']++;
+            }
+            String  key = Arrays.toString(distribution);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+
+    }
+}
