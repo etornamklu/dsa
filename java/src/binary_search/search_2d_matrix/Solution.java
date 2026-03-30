7077e1153d34d7a6527b889806f81a45adc2796e
etornamklu <03etornam@gmail.com>
2026-03-30 07:49:48 +0000

    Implement binary search algorithm for 1D and 2D matrices

---
+++ java/src/binary_search/search_2d_matrix/Solution.java
@@ -0,0 +1,24 @@
+package binary_search.search_2d_matrix;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int r = 0;
+        int c = n - 1;
+
+        while ( r < m && c >= 0) {
+            if (matrix[r][c] < target) {
+                r++;
+            } else if (matrix[r][c] > target) {
+                c--;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
