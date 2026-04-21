122eea87ecc91123998f9e4abd625a4d45992018
etornamklu <03etornam@gmail.com>
2026-04-21 14:59:52 +0000

    Implement solution to find minimum in rotated sorted array

---
+++ java/src/binary_search/min_in_rotated_sorted_array/Solution.java
@@ -0,0 +1,27 @@
+package binary_search.min_in_rotated_sorted_array;
+
+// 153. Find Minimum in Rotated Sorted Array 
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+// - [4,5,6,7,0,1,2] if it was rotated 4 times.
+// - [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[r]) {
+              r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return nums[l];
+        
+    }
+}
